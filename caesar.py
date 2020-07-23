ed2237c75b5e95b810d7fd818cf895ee56cc9ed0
omh18 <46792269+omh18@users.noreply.github.com>
2020-07-23 10:19:16 +0100

    Create caesar.py
    Simple caesar cipher encryption script

---
+++ caesar.py
@@ -0,0 +1,43 @@
+# Caesar Cipher Encryption Script
+
+key = int(input('Please enter a key: '))
+message = input('Please enter a message to encrypt: ')
+    
+key = int(key)
+
+encrypted_message = ' '
+ 
+for letter in message:
+
+  if letter.isalpha():
+  
+    ascii = ord(letter)
+    ascii += (key % 26)
+
+
+    if letter.isupper():
+
+      if ascii > ord('Z'):
+
+        ascii -= 26
+
+      elif ascii < ord('A'):
+
+        ascii += 26
+
+    elif letter.islower():
+
+      if ascii > ord('z'):
+
+        ascii -= 26
+
+      elif ascii < ord('a'):
+
+        ascii += 26
+
+    encrypted_message += chr(ascii)
+
+  else:
+    encrypted_message += letter
+
+print('The encrypted message is ' + encrypted_message)
